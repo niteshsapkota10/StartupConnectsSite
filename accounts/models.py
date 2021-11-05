8a79c4e7b4a355940c5da55c46a7169f75ef9feb
nitesh <lionitesh10@gmail.com>
2021-11-05 22:23:02 +0545

    [ProfilePictures Model Added in Accounts]

--- accounts/models.py
+++ accounts/models.py
@@ -32,3 +32,7 @@ class UserInfos(models.Model):
     intrests=models.CharField(max_length=1,choices=Interests,blank=False,null=False)
     user=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     others=models.TextField(blank=False,null=False,default="Add Others Field...")
+
+class ProfilePictures(models.Model):
+    picture=models.CharField(max_length=256,blank=False,null=False,default="default.png");
+    user=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
